68dc38af51321d7519f2e80529143d4b45f8cedf
Roy <56088145+royOST@users.noreply.github.com>
2020-04-16 22:53:07 -0400

    Initial Commit

---
+++ funny-noise-lol/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  funny-noise-lol
+//
+//  Created by Roy Smith on 4/16/20.
+//  Copyright © 2020 Roy Smith. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
